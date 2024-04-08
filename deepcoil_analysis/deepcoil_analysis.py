e9c87053664d18ca60bccf752884c0038dd915f3
DonEnno <enno.belz@outlook.com>
2024-04-08 15:34:14 +0200

    Now running

--- deepcoil_analysis/deepcoil_analysis.py
+++ deepcoil_analysis/deepcoil_analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 from Bio import SeqIO
 from deepcoil import DeepCoil
+import time
 
 data = '/ebio/abt1_share/prediction_hendecads/data/new_prot_fam_data/stretches.fasta'
 
@@ -11,9 +12,10 @@ dc = DeepCoil(use_gpu=False)
 
 result_df = pd.DataFrame(columns=['id', 'sequence', 'cc', 'hept'])
 
-CHUNK = 1000
+CHUNK = 100
 
-for i in range(0, len(inp), CHUNK):
+for i in range(0, 1000, CHUNK):
+    t0 = time.time()
     print(f"Processing sequences {i+1}-{min(i+CHUNK, len(inp))}", end='\r')
 
     tmp_keys = list(inp.keys())[i:i+CHUNK]
@@ -24,4 +26,6 @@ for i in range(0, len(inp), CHUNK):
     for k in tmp_keys:
         result_df = result_df.append({'id': k, 'sequence': inp[k], 'cc': preds[k]['cc'], 'hept': preds[k]['hept']}, ignore_index=True)
 
-result_df.to_csv('/ebio/abt1_share/prediction_hendecads/repo/deepcoil_analysis/deepcoil_results.csv', index=False)
+    print(f"Processing sequences {i+1}-{min(i+CHUNK, len(inp))} took {time.time()-t0:.2f} seconds")
+
+result_df.to_csv('/ebio/abt1_share/prediction_hendecads/repo/deepcoil_analysis/deepcoil_results_backup.csv', index=False)
