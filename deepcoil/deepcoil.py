8526c29985a0172ac6f86213d716ecaacdf48cc8
DonEnno <enno.belz@outlook.com>
2024-04-08 10:18:53 +0200

    Stash

---
+++ deepcoil/deepcoil.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from Bio import SeqIO
+
+from deepcoil import DeepCoil
+from deepcoil.utils import plot_preds
+
