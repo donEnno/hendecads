c8cfbd9abd0d6fab08a415490688d79b0f34ca73
DonEnno <enno.belz@outlook.com>
2024-04-24 15:48:36 +0200

    Transfer

--- 02_ss_pred/ss_pred.sh
+++ 02_ss_pred/ss_pred.sh
@@ -6,6 +6,8 @@ DRIVER_DIR="/ebio/abt3_projects/software"
 
 . /ebio/abt1_share/prediction_hendecads/conda/miniconda3/etc/profile.d/conda.sh
 conda activate tf
+HOME_DIR="/ebio/abt1_share/surfaceome"
+DRIVER_DIR="/ebio/abt3_projects/software" 
 
 export PATH="$PATH:$DRIVER_DIR/cuda-11.7/bin"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$DRIVER_DIR/cuda-11.7/lib64:$DRIVER_DIR/cuda-drivers" 
