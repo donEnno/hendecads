9a811d5acbf3f442d1749a1121fa3da656f698d2
DonEnno <enno.belz@outlook.com>
2024-04-26 14:30:48 +0200

    Blast UMAP

---
+++ 1_seq_analysis/blast_umap.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import umap
+
+df = pd.read_csv('/ebio/abt1_share/prediction_hendecads/3_CLANS/nxnblast', sep='\t', header=None)
+df.columns = ['i', 'j', 'score']
+
+# Create a matrix
+matrix = np.zeros((df['i'].max(), df['j'].max()))
+
+for i, j, score in df.values:
+    matrix[int(i), int(j)] = score
+
+# Standardize the matrix
+scaler = StandardScaler()
+matrix = scaler.fit_transform(matrix)
+
+# UMAP
+print('UMAP')
+reducer = umap.UMAP()
+embedding = reducer.fit_transform(matrix)
+
+# Plot
+print('Plot')
+plt.scatter(embedding[:, 0], embedding[:, 1], s=1)
+plt.savefig('/ebio/abt1_share/prediction_hendecads/1_repo/1_seq_analysis/umap.png')
---
+++ 1_seq_analysis/blast_umap.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+. /ebio/abt1_share/prediction_hendecads/conda/miniconda3/etc/profile.d/conda.sh
+conda activate seq_analysis
+
+python /ebio/abt1_share/prediction_hendecads/1_repo/1_seq_analysis/blast_umap.py
