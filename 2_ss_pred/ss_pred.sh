b23a78aa17ac1920294250dfd4996263868c9784
Enno <enno.belz@outlook.com>
2024-05-03 12:35:12 +0200

    Tried to resolve TF issues

--- 2_ss_pred/ss_pred.sh
+++ 2_ss_pred/ss_pred.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 # Variables
-HOME_DIR="/ebio/abt1_share/surfaceome"
-DRIVER_DIR="/ebio/abt3_projects/software" 
 
 . /ebio/abt1_share/prediction_hendecads/conda/miniconda3/etc/profile.d/conda.sh
 conda activate tf
-HOME_DIR="/ebio/abt1_share/surfaceome"
+
+HOME_DIR="/ebio/abt1_share/prediction_hendecads"
 DRIVER_DIR="/ebio/abt3_projects/software" 
 
 export PATH="$PATH:$DRIVER_DIR/cuda-11.7/bin"
