8dbd39951d666d95072ea10fdc6797cf96883c09
DonEnno <enno.belz@outlook.com>
2024-05-06 09:16:25 +0200

    GPU works now

--- 1_seq_analysis/blast_umap.py
+++ 1_seq_analysis/blast_umap.py
@@ -4,19 +4,24 @@ import matplotlib.pyplot as plt # type: ignore
 from sklearn.preprocessing import StandardScaler # type: ignore
 import umap # type: ignore
 
+print("Let's go")
 df = pd.read_csv('/ebio/abt1_share/prediction_hendecads/3_CLANS/nxnblast', sep='\t', header=None)
 df.columns = ['i', 'j', 'score']
 
+print("Start matrix")
 # Create a matrix
 matrix = np.zeros((df['i'].max()+1, df['j'].max()+1))
 
 for i, j, score in df.values:
     matrix[int(i), int(j)] = score
+    matrix[int(i), int(j)] = score
+
+print("Matrix done")
 
 # Standardize the matrix
 scaler = StandardScaler()
 matrix = scaler.fit_transform(matrix)
-
+print("Scaler done")
 # UMAP
 print('UMAP')
 reducer = umap.UMAP()
