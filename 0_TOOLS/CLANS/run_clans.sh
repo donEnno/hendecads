5a12f9598a6e19b984978e5a4c78620742b077c0
Enno <enno.belz@outlook.com>
2024-05-21 15:55:46 +0200

    3R 0MM clans setup

--- 0_TOOLS/CLANS/run_clans.sh
+++ 0_TOOLS/CLANS/run_clans.sh
@@ -2,6 +2,6 @@ INPUT_FILE=$1
 PVAL=$2
 
 BASE_NAME=$(basename $INPUT_FILE)
-OUTPUT_FILE="${BASE_NAME}_${PVAL}"
+OUTPUT_FILE="${BASE_NAME}_${PVAL}.clans"
 
 java -Xmx8G -jar CLANSclans_20170324.jar -load $INPUT_FILE -cpu 12 -nographics true -dorounds 3000 -pval $PVAL -cluster2d true -saveto /home/enno/uni/SS24/thesis/1_RegEx/1_TMP/clans_out/$OUTPUT_FILE -initialize false
