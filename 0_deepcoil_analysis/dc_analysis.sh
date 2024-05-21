58b9d6d78b3bdc1e2a0ad3d066d5ec77610551d9
DonEnno <enno.belz@outlook.com>
2024-05-21 11:57:09 +0200

    Clean MPI structure

--- 0_deepcoil_analysis/dc_analysis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-. /ebio/abt1_share/prediction_hendecads/conda/miniconda3/etc/profile.d/conda.sh
-conda activate deepCoil
-
-python /ebio/abt1_share/prediction_hendecads/repo/deepcoil_analysis/deepcoil_analysis.py
